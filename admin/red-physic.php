2e1a1e89ed51f5dcad7e107a46f607315218f05d
RapidDev <kontakt@rapiddev.pl>
2019-11-10 23:32:34 +0100

    Disable cache

--- admin/red-physic.php
+++ admin/red-physic.php
@@ -265,6 +265,11 @@
 			$this->js_redirect($record->url);
 
 			//Redirect
+			header("Expires: on, 01 Jan 1970 00:00:00 GMT");
+			header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+			header("Cache-Control: no-store, no-cache, must-revalidate");
+			header("Cache-Control: post-check=0, pre-check=0", false);
+			header("Pragma: no-cache");
 			header('HTTP/1.1 301 Moved Permanently');
 			header('Location: ' . $record->url);
 			exit;	
