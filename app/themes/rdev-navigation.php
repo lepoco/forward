ca33220b80f1332e0751e9fc04f970ecf8494340
Pomianowski <kontakt@rapiddev.pl>
2021-01-19 18:09:06 +0100

    Disable profile settings for now

--- app/themes/rdev-navigation.php
+++ app/themes/rdev-navigation.php
@@ -50,6 +50,17 @@
 								</a>
 							</li>
 							*/ ?>
+							<li class="nav-item">
+								<a class="nav-link nav-link-icon<?php echo $this->name == 'settings' ? ' active' : '' ?>" href="<?php echo $dashboard . 'settings/'; ?>">
+									<!--
+									<svg viewBox="0 0 16 16" class="bi bi-columns-gap" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+										<path fill-rule="evenodd" d="M6 1H1v3h5V1zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1H1zm14 12h-5v3h5v-3zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5zM6 8H1v7h5V8zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H1zm14-6h-5v7h5V1zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1h-5z"/>
+									</svg>
+									-->
+									<?php $this->_e('Settings'); ?>
+								</a>
+							</li>
+							<?php /*
 							<li class="nav-item dropdown">
 								<a class="nav-link dropdown-toggle nav-link-icon<?php echo $this->name == 'settings' ? ' active' : '' ?>" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
 									<!--
@@ -69,6 +80,7 @@
 									<li><a class="dropdown-item" href="<?php echo $dashboard . 'about/'; ?>"><?php $this->_e('About'); ?></a></li>
 								</ul>
 							</li>
+							*/ ?>
 							<li class="nav-item dropdown">
 								<a class="nav-link dropdown-toggle nav-link-icon" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
 									<!--
@@ -82,7 +94,7 @@
 								</a>
 								<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 									<li>
-										<a class="dropdown-item" href="<?php echo $dashboard . 'user/' . $this->Forward->User->Active()['user_id']; ?>">
+										<a class="dropdown-item disabled" disabled="disabled" href="<?php echo $dashboard . 'user/' . $this->Forward->User->Active()['user_id']; ?>">
 										Profile settings
 									</a>
 									</li>
